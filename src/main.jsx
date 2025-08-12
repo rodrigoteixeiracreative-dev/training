2ab9fbcaff0635b69eb5d5363b2744155acc369d
rodrigoteixeiracreative-dev <rodrigoteixeiracreative@gmail.com>
2025-08-12 18:17:44 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1 style={{textAlign:'center',marginTop:'40px'}}>Sirius Training</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
